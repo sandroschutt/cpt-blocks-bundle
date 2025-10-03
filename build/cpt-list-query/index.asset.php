de794c6472d9657a83ccb97941137a68d435c8de
sandroschutt <dev.sandroschutt@gmail.com>
2025-10-03 20:33:09 -0300

    Remove unnecessary blocks

--- build/cpt-list-query/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '8e68d258302e7b696f56');
--- build/cpt-query-acf-fields/view.js
+++
@@ -1,1 +0,0 @@
-console.log("Hello World! (from create-block-cpt-query-acf-fields block)");
